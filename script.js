462a24c5e84f640f496be5d7f19daaf4f8366f4e
FlaviaRamosdaSilva <didi.flavia@gmail.com>
2023-06-08 18:05:12 -0300

    perto-do-fim

--- script.js
+++ script.js
@@ -1,13 +1,38 @@
 const convertButton = document.querySelector("button")
+const currencySelect = document.querySelector(".currency-select")
 
-function ConvertValues() {
+console.log(currencySelect)
+
+function convertValues() {
     const inputCurrencyValue = document.querySelector(".input-currency").value
-
+    const currencyValueToConvert = document.querySelector("#real")
+    const currencyValueToConverted = document.querySelector("#dolar")
+    
     const dolarToday = 5.2
+    const euroToday = 6.2
+    const libraToday = 8.0
 
-    const convertedValue = inputCurrencyValue / dolarToday
-
-    console.log(convertedValue)
+if(currencySelect.value == "dolar") {
+    currencyValueToConvert.innerHTML = new Intl.NumberFormat("en-US" , {
+        style: "currency" ,
+        currency: "USD"
+    }).format(inputCurrencyValue/dolarToday)
 }
 
-convertButton.addEventListener("click", ConvertValues)
+if(currencySelect.value == "euro") {
+    currencyValueToConverted.innerHTML = new Intl.NumberFormat("de-DE" , {
+        style: "currency" ,
+        currency: "EUR"
+    }).format(inputCurrencyValue/euroToday)
+}
+
+
+currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+    style: "currency" ,
+    currency: "BRL"
+}).format(inputCurrencyValue)
+
+}
+    
+
+convertButton.addEventListener("click", convertValues)
